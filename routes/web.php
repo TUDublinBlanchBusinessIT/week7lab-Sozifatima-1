40971a3a72d94e5bed6a8f35a63484ceaad3c641
B00158031 Sozi Fatima <B00158031@mytudublin.ie>
2025-03-10 16:19:39 +0000

    part 2

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+Route::resource('members', App\Http\Controllers\memberController::class);
+
+
+Route::resource('courts', App\Http\Controllers\courtController::class);
+
+
+Route::resource('bookings', App\Http\Controllers\bookingController::class);
